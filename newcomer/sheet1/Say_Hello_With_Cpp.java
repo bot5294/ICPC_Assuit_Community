edfa6590bf712990ace32b1feda2881a95416393
bot5294 <5294aqib@gmail.com>
2022-11-12 01:14:23 +0530

    Say Hello with C++ Added

---
+++ newcomer/sheet1/Say_Hello_With_Cpp.java
@@ -0,0 +1,13 @@
+package newcomer.sheet1;
+
+//  Problem link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+import java.util.Scanner;
+
+public class Say_Hello_With_Cpp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, " + sc.next());
+    }
+}
