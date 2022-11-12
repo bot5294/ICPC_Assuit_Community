a17539781e9629c0482a0105aa5af2f47a41582c
bot5294 <5294aqib@gmail.com>
2022-11-12 16:46:45 +0530

    A_1_to_N.java added

---
+++ newcomer/sheet2_loops/A_1_to_N.java
@@ -0,0 +1,14 @@
+import java.util.*;
+/**
+ * A_1_to_N
+ */
+public class A_1_to_N {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
