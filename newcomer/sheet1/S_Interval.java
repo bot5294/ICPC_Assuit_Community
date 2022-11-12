805565ca680e9e6201321d86736af7c3e62c5824
bot5294 <5294aqib@gmail.com>
2022-11-12 06:15:00 +0530

    S_Interval.java added

---
+++ newcomer/sheet1/S_Interval.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/**
+ * S_Interval
+ */
+public class S_Interval {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float x = sc.nextFloat();
+        if(x>=0 && x<=25){
+            System.out.println("Interval [0,25]");
+        }else if(x>25 && x<=50){
+            System.out.println("Interval (25,50]");
+        }else if(x>50 && x<=75){
+            System.out.println("Interval (50,75]");
+        }else if(x>75 && x<=100){
+            System.out.println("Interval (75,100]");
+        }else{
+            System.out.println("Out of Intervals");
+        }
+        sc.close();
+    }
+}
