1e702e6f1c0c5fd803db24f6815542fc165ee0e4
bot5294 <5294aqib@gmail.com>
2022-11-13 15:49:16 +0530

    J_Primes_from_1_to_n.java added

---
+++ newcomer/sheet2_loops/J_Primes_from_1_to_n.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class J_Primes_from_1_to_n{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i=2;i<=n;i++){
+            boolean isPrime=true;
+            for(int j=2;j*j<=i;j++){
+                if(i%j==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.print(i+" ");
+            }
+        }
+        sc.close();
+    }
+}
