36e462ce57399c56f74705c36f81d95544d87a69
bot5294 <5294aqib@gmail.com>
2022-11-14 17:22:54 +0530

    Q_Digits.java added

--- newcomer/sheet2_loops/Q_Digits.java
+++ newcomer/sheet2_loops/Q_Digits.java
@@ -4,10 +4,19 @@ public class Q_Digits{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         while(n>0){
-            String d = sc.next();
-            for(int i=d.length()-1;i>=0;i--){
-                System.out.print(d.charAt(i)+" ");
+            long d = sc.nextLong();
+            if(d==0) {
+                System.out.print(d);
             }
+            while(d>0){
+                System.out.print(d%10+" ");
+                d=d/10;
+            }
+            // string method
+            // String d = sc.next();
+            // for(int i=d.length()-1;i>=0;i--){
+            //     System.out.print(d.charAt(i)+" ");
+            // }
             System.out.println();
             n--;
         }
