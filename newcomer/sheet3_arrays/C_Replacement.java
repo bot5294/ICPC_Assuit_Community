3ca9f55c072a2a1d14bd2d4014a45b50df402c88
bot5294 <5294aqib@gmail.com>
2022-11-16 03:22:55 +0530

    C_Replacement.java added

---
+++ newcomer/sheet3_arrays/C_Replacement.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class C_Replacement{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                arr[i]=1;
+            }else if(arr[i]<0){
+                arr[i]=2;
+            }
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
