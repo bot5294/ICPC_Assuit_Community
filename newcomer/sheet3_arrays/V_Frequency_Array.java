688d7484037da61dc5be283e5bc8185c1ff0762b
bot5294 <5294aqib@gmail.com>
2022-11-24 00:15:10 +0530

    V_Frequency_Array.java added

--- newcomer/sheet3_arrays/V_Frequency_Array.java
+++ newcomer/sheet3_arrays/V_Frequency_Array.java
@@ -4,15 +4,19 @@ public class V_Frequency_Array{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int max = Math.max(n,m);
-        int freq[] = new int[max+1];
+        // int max = Math.max(n,m);
+        int freq[] = new int[m+1];
+        int arr[] = new int[n];
         for(int i=0;i<n;i++){
-            int val = sc.nextInt();
-            freq[val]= freq[val]==0?1:freq[val]+1;
+            arr[i] = sc.nextInt();
+            if(freq[arr[i]]==0){
+                freq[arr[i]]=1;
+            }else{
+                freq[arr[i]]+=1;
+            }
         }
         for(int j=1;j<=m;j++){
-            if(freq[j]>0)
-                System.out.println(freq[j]);
+            System.out.println(freq[j]);
         }
         sc.close();
     }
