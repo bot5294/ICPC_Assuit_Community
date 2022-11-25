6cb35031a7a45a123f03748343891287879f2555
bot5294 <5294aqib@gmail.com>
2022-11-25 16:44:46 +0530

    A_Create_A_New_String.java added

---
+++ newcomer/sheet4_strings/A_Create_A_New_String.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class A_Create_A_New_String{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+        System.out.println(s.length()+" "+t.length());
+        System.out.println(s+" "+t);
+        sc.close();
+    }
+}
