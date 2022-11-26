f6630ac3226a506b17ed9dc9880df4431835d46c
bot5294 <5294aqib@gmail.com>
2022-11-26 18:08:42 +0530

    X_8_Neighbors.java added

--- newcomer/sheet3_arrays/X_8_Neighbors.java
+++ newcomer/sheet3_arrays/X_8_Neighbors.java
@@ -8,59 +8,26 @@ public class X_8_Neighbors{
         for(int i=0;i<n;i++){
             String s= sc.next();
             for(int j=0;j<m;j++){
-                arr[i][j]=s.charAt(j);    
+                arr[i][j]=s.charAt(j); 
             }
         }
         int r = sc.nextInt()-1;
         int c =sc.nextInt()-1;
-        boolean hasEightN = true;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==r && j==c){
-                    if(i-1>=0 && j-1>=0)
-                    if(arr[i-1][j-1]!='x'){
-                        hasEightN=false;
-                        break;
-                    }
-                    if(i-1>=0)
-                    if(arr[i-1][j]!='x'){
-                        hasEightN=false;
-                        break;
-                    }
-                    if(i-1>=0 && j+1<m)
-                    if(arr[i-1][j+1]!='x'){
-                        hasEightN=false;
-                        break;
-                    }
-                    if(j-1>=0)
-                    if(arr[i][j-1]!='x'){
-                        hasEightN=false;
-                        break;
-                    }
-                    if(j+1<m)
-                    if(arr[i][j+1]!='x'){
-                        hasEightN=false;
-                        break;
-                    }
-                    if(j-1>=0 && i+1<n)
-                    if(arr[i+1][j-1]!='x'){
-                        hasEightN=false;
-                        break;
-                    }
-                    if(i+1<n)
-                    if(arr[i+1][j]!='x'){
-                        hasEightN=false;
-                        break;
-                    }
-                    if(i+1<n && j+1<m)
-                    if(arr[i+1][j+1]!='x'){
-                        hasEightN=false;
-                        break;
-                    }
-                }
+        int ix[] = {-1,-1,-1,0,0,1,1,1};
+        int iy[] = {-1,0,1,-1,1,-1,0,1};
+        for(int i=0;i<8;i++){
+            int x = r+ix[i];
+            int y = c+iy[i]; 
+            if(x>=0 && x<n && y>=0 && y<m)
+            {
+            if(arr[x][y]!='x'){
+                System.out.println("no");
+                sc.close();
+                return;
             }
         }
-        System.out.println(hasEightN==true?"yes":"no");
+        }
+        System.out.println("yes");
         sc.close();
     }
 }
