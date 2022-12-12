019a9ad9775cefdd6c3282da245d251d8fc65db4
bot5294 <5294aqib@gmail.com>
2022-12-12 15:41:42 +0530

    C_Sum_of_Range.java added

--- newcomer/sheet6_maths/C_Sum_of_Range.java
+++ newcomer/sheet6_maths/C_Sum_of_Range.java
@@ -1,10 +1,14 @@
+// sum of n odd numbers n^2
+//  sum of n even numbers  n(n+1)
+//  sum of n natural numbers  n(n-1)/2
+
 import java.util.*;
 public class C_Sum_of_Range{
     public static long sumN(long n){
         return (n*(n+1));
     }
     public static long sumEven(long l,long r){
-        return sumN(r/2)-sumN((l-1)/2);
+        return sumN(r/2)-sumN((l-1)/2); // passing after divding by 2 beacuse we have 1/2 numbers as even
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
